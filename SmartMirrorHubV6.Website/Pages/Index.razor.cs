63b81afdba6fae329cdf93a23fb74b51db34ef99
Gary Fung <xzblaze@hotmail.com>
2022-10-01 22:59:40 +1300

    Fix if debug in website

--- SmartMirrorHubV6.Website/Pages/Index.razor.cs
+++ SmartMirrorHubV6.Website/Pages/Index.razor.cs
@@ -1,20 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿namespace SmartMirrorHubV6.Website.Pages;
 
-namespace SmartMirrorHubV6.Website.Pages
+public partial class Index : BaseComponent
 {
-    public partial class Index : BaseComponent
+    protected override void OnInitialized()
     {
-        protected override void OnInitialized()        
-        {
-            base.OnInitialized();
+        base.OnInitialized();
 #if DEBUG
-            NavigationManager.NavigateTo("/mirror/1/Dev");
+        NavigationManager.NavigateTo("/mirror/1/Dev");
 #else
-            NavigationManager.NavigateTo($"/mirror/{Settings.UserId}/{Settings.MirrorName}");
+        NavigationManager.NavigateTo($"/mirror/{Configuration["UserId"]}/{Configuration["MirrorName"]}");
 #endif
-        }
     }
 }
